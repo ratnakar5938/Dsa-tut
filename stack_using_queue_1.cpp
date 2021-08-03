79ee696214b2cfab88dd797b07deb82a699a60c8
Ratnakar Sahoo <ratnakar3895@yahoo.com>
2021-08-04 00:12:05 +0530

    stack using queues - costly push method

---
+++ stack_using_queue_1.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+class Stack
+{
+    int N;
+    queue<int> q1;
+    queue<int> q2;
+
+public:
+    Stack()
+    {
+        N = 0;
+    }
+
+    void push(int);
+    void pop();
+    int top();
+    int size();
+};
+
+void Stack::push(int val)
+{
+    q2.push(val);
+    N++;
+    while (!q1.empty())
+    {
+        q2.push(q1.front());
+        q1.pop();
+    }
+
+    queue<int> temp = q1;
+    q1 = q2;
+    q2 = temp;
+}
+
+void Stack::pop()
+{
+    q1.pop();
+    N--;
+}
+
+int Stack::top()
+{
+    return q1.front();
+}
+
+int Stack::size()
+{
+    return N;
+}
+
+int main()
+{
+    Stack st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+
+    cout << st.top() << endl;
+    st.pop();
+    cout << st.top() << endl;
+    st.pop();
+    cout << st.top() << endl;
+    st.pop();
+    cout << st.top() << endl;
+    st.pop();
+
+    cout << endl
+         << st.size() << endl;
+
+    return 0;
+}
