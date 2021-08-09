5ec8ca58ea916e9e3c8d7886cb28efa3288153a3
Ratnakar Sahoo <ratnakar3895@yahoo.com>
2021-08-09 14:22:31 +0530

    Maximum Consequitive ones

---
+++ maximum_consequetive_ones.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int k;
+    cin >> k;
+    vector<int> a(n);
+
+    for (auto &i : a)
+        cin >> i;
+
+    int zerocnt = 0, i = 0, ans = 0;
+    for (int j = 0; j < n; j++)
+    {
+        if (a[j] == 0)
+            zerocnt++;
+
+        while (zerocnt > k)
+        {
+            if (a[i] == 0)
+            {
+                zerocnt--;
+            }
+            i++;
+        }
+        ans = max(ans, j - i + 1);
+    }
+    cout << ans << endl;
+
+    return 0;
+}
